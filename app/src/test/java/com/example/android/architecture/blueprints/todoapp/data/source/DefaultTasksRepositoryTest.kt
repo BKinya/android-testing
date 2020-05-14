b51fcf1ed495f8cf8c49bb8bcf5b9dcea88cba38
kinya <gitongakinya1@gmail.comj>
2020-05-14 21:24:10 +0300

    More tests

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -1,5 +1,6 @@
 package com.example.android.architecture.blueprints.todoapp.data.source
 
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
 import com.example.android.architecture.blueprints.todoapp.data.Result
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import kotlinx.coroutines.Dispatchers
@@ -8,6 +9,7 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.core.IsEqual
 import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
@@ -25,6 +27,9 @@ class DefaultTasksRepositoryTest{
     // Class under test
     private lateinit var tasksRepository: DefaultTasksRepository
 
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
     @Before
     fun setUpRepository(){
         tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
@@ -34,12 +39,12 @@ class DefaultTasksRepositoryTest{
         tasksRepository = DefaultTasksRepository(
                 tasksRemoteDataSource,
                 tasksLocalDataSource,
-                Dispatchers.Unconfined
+                Dispatchers.Main
         )
     }
 
     @Test
-    fun getTasks_requestsAllTasksFromRemoteDataSource() = runBlockingTest {
+    fun getTasks_requestsAllTasksFromRemoteDataSource() = mainCoroutineRule.runBlockingTest {
         val tasks = tasksRepository.getTasks(true) as Result.Success
 
         assertThat(tasks.data, IsEqual(remoteTasks))
